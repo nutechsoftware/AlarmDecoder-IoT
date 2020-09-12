c8634b24eafc4b83702aa7d160748424548b00f8
f34rdotcom <coder@f34r.com>
2020-09-12 10:36:31 -0700

    refactor configs and rename key files to clerify use.

--- main/component.mk
+++ main/component.mk
@@ -3,4 +3,4 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_EMBED_TXTFILES := onboarding_config.json device_info.json root.pem public_key.pem
+COMPONENT_EMBED_TXTFILES := onboarding_config.json device_info.json update_root.pem update_public_key.pem
