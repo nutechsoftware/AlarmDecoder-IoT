219c071a9c4391c6e4c21a9eb5683787f9557490
f34rdotcom <coder@f34r.com>
2020-09-16 08:40:23 -0700

    Meh.. missed this

---
+++ main/ad2_settings.h
@@ -0,0 +1,36 @@
+/**
+ *  @file    ad2_settings.h
+ *  @author  Sean Mathews <coder@f34r.com>
+ *  @date    02/20/2020
+ *  @version 1.0
+ *
+ *  @brief Settings common to everyone
+ *
+ *  @copyright Copyright (C) 2020 Nu Tech Software Solutions, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+// configuration
+
+#define AD2_MAX_ADDRESS       99
+#define AD2_MAX_VPARTITION    9
+#define AD2_MAX_CODE          99
+#define AD2_DEFAULT_CODE_SLOT 0
+#define AD2_DEFAULT_VPA_SLOT  0
+#define AD2_MAX_MODE_ARG_SIZE 80
+
+#define VPADDR_CONFIG_KEY     "vpa"
+#define CODES_CONFIG_KEY      "codes"
+#define AD2MODE_CONFIG_KEY    "ad2source"
