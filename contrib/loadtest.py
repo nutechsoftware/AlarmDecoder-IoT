019d7833591d404a9c4070713b8d4230f8189924
f34rdotcom <coder@f34r.com>
2021-08-18 21:18:30 -0700

    Load testing demo updating two web browsers with different partitions and MQTT server each time a partition changes from ready to not ready.

--- contrib/loadtest.py
+++ contrib/loadtest.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+#!/usr/bin/python
 # AlarmDecoder IoT load testing.
 # Open/Close relays tied to ZONE's on a test panel
 # READY/FAULT on mulitple partitions infinite loop
@@ -11,6 +11,13 @@ commands = [
     "4112#702", # OPEN RELAY 2
     "4112#802"  # CLOSE RELAY 2
 ]
+comments = [
+"OPEN RELAY 1 FAULTING ZONE 3 on Partition 2",
+"CLOSE RELAY 1 RESTORING ZONE 3 on Partition 2",
+"OPEN RELAY 2 FAULTING ZONE 2 on Partition 1",
+"OPEN RELAY 2 RESTORING ZONE 2 on Partition 1",
+]
+
 s = socket.socket()
 
 # Connect to AlarmDecoder IoT ser2sock port
@@ -18,6 +25,9 @@ s.connect(("192.168.3.120", 10000))
 
 # Infinite loop CTL+C to break
 while True:
-  for command in commands:
-      s.send(command.encode("utf8"))
-      time.sleep(3)
+  for i in range(len(commands)):
+      print("sending: " + commands[i] + " - " + comments[i])
+
+      s.send(commands[i].encode("utf8"))
+      s.recv(1024)
+      time.sleep(5)
