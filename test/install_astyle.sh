1ccef703222028bf713c2576e6c9b253d7e24775
f34rdotcom <coder@f34r.com>
2021-10-12 20:00:47 -0700

    Temp hack to fix astyle. Maybe a better way exists to install this package. One that will hold up better over time.

--- test/install_astyle.sh
+++ test/install_astyle.sh
@@ -8,7 +8,7 @@
 set -e
 
 if [ ! -f $HOME/astyle/build/gcc/bin/astyle ]; then
-	wget -O astyle_3.1_linux.tar.gz https://sourceforge.net/projects/astyle/files/astyle/astyle%203.1/astyle_3.1_linux.tar.gz/download
+	wget --no-check-certificate -O astyle_3.1_linux.tar.gz https://sourceforge.net/projects/astyle/files/astyle/astyle%203.1/astyle_3.1_linux.tar.gz/download
 	tar -xf astyle_3.1_linux.tar.gz -C $HOME
 	make -C $HOME/astyle/build/gcc
 fi
